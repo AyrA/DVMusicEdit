92b20f210df3cf797bcf8dee45282df2f7686bca
Kevin Gut <kevin.gut@bitm.sg>
2021-09-13 00:20:30 +0200

    Update Version

--- DVMusicEdit/Properties/AssemblyInfo.cs
+++ DVMusicEdit/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DVMusicEdit")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Derail Valley Playlist Editor")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("AyrA.ch")]
 [assembly: AssemblyProduct("DVMusicEdit")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright © AyrA.ch 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
