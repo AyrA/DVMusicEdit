6ae895afe05ce3f5056c8e2caca92ba2a4f5feb7
Kevin Gut <kevin.gut@bitm.sg>
2021-09-13 00:00:10 +0200

    Cleanup solution

--- DVMusicEdit/Ffmpeg.cs
+++ DVMusicEdit/Ffmpeg.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Security;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DVMusicEdit
 {
